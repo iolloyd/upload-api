8dc23cfb0a6253448aa9608d16b2d8ff97d9179b
lloyd <manchesterboy@gmail.com>
2014-04-08 12:00:02 +0200

    add endpoint to test queueing

--- public_html/routes/videos.php
+++ public_html/routes/videos.php
@@ -1,34 +1,74 @@
 <?php
+require_once "../src/FileUploader.php";
+
+use Cloud\FileUploader as FileUploader;
 
 function getVideos() {
     $videos = R::findAll('video');
     return $videos;
 }
 
+function getVideo($id) {
+    $video = R::find('video', $id);
+    return $video;
+}
 
-$app->get('/videos', function() use ($app) {
-    $videos = getVideos();
-    $app->json(R::exportAll($videos));
-    //$app->render('list.html', array('videos' => $videos));
-});
+function saveVideoData($app, $title, $description, $path) {
+    $user = $app->user;
+    $video = R::dispense('video');
+    $video->title = $title;
+    $video->description = $description;
+    $video->path = $path;
+    $user->ownVideoList[] = $video;
+    return R::store($video);
+}
 
+function saveVideoFile($uploadDir, $info) {
+    $from = $info['tmp_name'];
+    $to = "$uploadDir/{$info['name']}";
+    $ok = move_uploaded_file($from, $to);
+    if (!$ok) {
+        return false;
+    }
+
+    return $to;
+}
+
+// Dev helper form
 $app->get('/videos/new', function() use ($app) {
     $app->render('new.html'); 
 });
 
+$app->get('/videos', function() use ($app) {
+    $videos = getVideos();
+    $app->json(R::exportAll($videos));
+});
+
 $app->get('/videos/:id', function($id) use ($app) {
     $data = array('id' => $id);
-    $app->render('video.html', $data); 
+    $app->json(getVideo($id));
 });
 
 $app->post('/videos/upload', function() use ($app, $config) {
-    $dbInfo = $config('db');
-    $info = $_POST;
-    $user = $app->user;
-    $video = R::dispense('video');
-    $video->import($_POST, 'title, description');
-    $id = R::store($video);
-    echo 'stored video info with id ' . $id . '</br>';
+    $uploadDir = $config('upload')['dir'];
+    $path = saveVideoFile($uploadDir, $_FILES['video']);
+    if ($path) {
+        $id = saveVideoData($app, $_POST['title'], $_POST['description'], $path);
+
+        echo 'stored video info with id ' . $id . ' in ' . $path . '</br>';
+
+    } else {
+        echo 'Oops, upload no workie';
+    }
+});
+
+$app->get('/videos/send/:id', function($id) use ($app, $config) {
+    $video = R::load('video', $id);
+    $redisBackend = $config('redis')['backend'];
+    Resque::setBackend($redisBackend);
+    $args = ['paysite' => $video->path];
+    $token = Resque::enqueue('video_upload', 'FileUploader', $args, true); 
+    echo $token;
 });
 
 $app->put('/videos/:id', function($id) use ($app) {
