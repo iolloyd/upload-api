f1ff2ee069742b16674aad780282d3d2d7c5b473
lloyd <manchesterboy@gmail.com>
2014-04-10 07:44:35 +0200

    pass filename to server

--- src/Cloud/Worker/FileUpload.php
+++ src/Cloud/Worker/FileUpload.php
@@ -10,15 +10,17 @@ class FileUpload
     {
         $this->upload(
             $this->args['source'], 
-            $this->args['destination']
+            $this->args['destination'],
+            $this->args['filename']
         );
     }
 
-    protected function upload($source, $destination)
+    protected function upload($source, $destination, $filename)
     {
         $client = new Client();
         $request = $client->createRequest('POST', $destination);
         $body = $request->getBody();
+        $body->setField('filename', $filename);
         $body->addFile(new PostFile('video', fopen($source, 'r')));
         $response = $client->send($request);
         return $response;
