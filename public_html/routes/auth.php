5b5d562048b02e79e38cce9e711a02edf073116d
lloyd <manchesterboy@gmail.com>
2014-04-10 18:58:30 +0200

    refactor auth middleware

--- public_html/routes/auth.php
+++ public_html/routes/auth.php
@@ -1,31 +1,35 @@
 <?php
 
-function notAuthenticated($email, $password) {
-    return $email != 'me' || $password != 'me';
+function authenticated($email, $password) {
+    $user = R::getRow(
+        'SELECT * FROM user WHERE email=:email',
+        ['email' => $email]
+    );
+
+    return $user && $user['email'] == $email;
 }
 
 $app->get('/login', function() use ($app) {
-    $app->render('login.html', array('error' => $app->flash));
+    $app->render('login.html', ['error' => $app->flash]);
 });
 
-$app->post("/login", function () use ($app) {
+$app->post("/login", function() use ($app) {
     $email = $app->request()->post('email');
     $password = $app->request()->post('password');
-    $errors = array();
 
-    if (notAuthenticated($email, $password)) {
-        $app->flash('error', 'Email or password not good');
+    if (authenticated($email, $password)) {
+        $_SESSION['user'] = $email;
+
+        if (isset($_SESSION['urlRedirect'])) {
+            $nextUrl = $_SESSION['urlRedirect'];
+            unset($_SESSION['urlRedirect']);
+            $app->redirect($nextUrl);
+        }
+        $app->redirect('/');
+    } else {
         $app->redirect('/login');
     }
 
-    $_SESSION['user'] = $email;
-
-    if (isset($_SESSION['urlRedirect'])) {
-        $tmp = $_SESSION['urlRedirect'];
-        unset($_SESSION['urlRedirect']);
-        $app->redirect($tmp);
-    }
-    $app->redirect('/');
 });
 
 $app->get('/logout', function() use ($app) {
