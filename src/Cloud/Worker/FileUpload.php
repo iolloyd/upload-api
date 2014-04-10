8c2e03f137e18049729c428bc25e0e6d0a3f018d
lloyd <manchesterboy@gmail.com>
2014-04-10 05:56:30 +0200

    fix guzzle method call

--- src/Cloud/Worker/FileUpload.php
+++ src/Cloud/Worker/FileUpload.php
@@ -1,20 +1,27 @@
 <?php
 namespace Cloud\Worker;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Post\Postfile;
+
 class FileUpload
 {
     public function perform()
     {
         $this->upload(
-            $this->args['source'],
+            $this->args['source'], 
             $this->args['destination']
         );
     }
 
-    protected function upload($args)
+    protected function upload($source, $destination)
     {
-        $client = new Guzzle\Http\Client($destination);
-        $client->put('/', null, fopen($source, 'r'))->send();
+        $client = new Client();
+        $request = $client->createRequest('POST', $destination);
+        $body = $request->getBody();
+        $body->addFile(new PostFile('video', fopen($source, 'r')));
+        $response = $client->send($request);
+        return $response;
     }
 }
 
