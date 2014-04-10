a16c63e99e099d1a0f92eac652a5a67208979bec
lloyd <manchesterboy@gmail.com>
2014-04-10 18:58:54 +0200

    remove functionality that will be in angular

--- public_html/routes/videos.php
+++ public_html/routes/videos.php
@@ -1,71 +1,6 @@
 <?php
 use Cloud\Worker\FileUpload;
 
-function getVideos() {
-    $videos = R::findAll('video');
-    return $videos;
-}
-
-function getVideo($id) {
-    $video = R::find('video', $id);
-    return $video;
-}
-
-function saveVideoData($app, $title, $description, $path) {
-    $user = $app->user;
-    $video = R::dispense('video');
-    $video->title = $title;
-    $video->description = $description;
-    $video->path = $path;
-    $user->ownVideoList[] = $video;
-    return R::store($video);
-}
-
-function saveVideoFile($uploadDir, $info) {
-    $from = $info['tmp_name'];
-    $to = "$uploadDir/{$info['name']}";
-    $ok = move_uploaded_file($from, $to);
-    if (!$ok) {
-        return false;
-    }
-
-    return $to;
-}
-
-function getAmazonEndpoint($config) {
-    $aws = $config('amazon');
-    $s3 = new S3($aws['access_key'], $aws['secret_key']);
-    /*
-    S3::putBucket($aws['bucket']);
-    $inputFile = S3::inputFile($file, false);
-    $contentType = getContentType($file);
-    S3::putObject($inputFile, $bucketName, $uploadName, S3::AUTHENTICATED_READ);
-     */
-}
-
-function getContentType($file) {
-    return 'video/mpeg';
-}
-
-function getEndpoint($bucket) {
-    return 'http://'.$bucket.'.amazonaws.com';
-}
-
-function getPolicy($bucket, $redirect, $contentType, $acl='public-read') {
-    $expires = "2015-12-01T12:00:00.000Z";
-    $policy = <<<POLICY
-        { "expiration": $expires,
-            "conditions": [
-                {"bucket": $bucket},
-                ["starts-with", "$key", "user/eric/"],
-                {"acl": $acl},
-                {"success_action_redirect": $redirect},
-                ["eq", "$Content-Type", $contentType]
-            ]
-        }
-POLICY;
-}
-
 function createJob($token) {
     $job = R::dispense('job');
     $job->token   = $token;
@@ -76,7 +11,7 @@ function createJob($token) {
     return $job;
 }
 
-function queueFileUpload($redisBackend, $video, $filename) {
+function queueFileUpload($redisBackend, $user, $video, $filename) {
     Resque::setBackend($redisBackend);
     $args = [
         'source' => $video->source,
@@ -89,57 +24,43 @@ function queueFileUpload($redisBackend, $video, $filename) {
     return $token;
 }
 
-// Dev helper form
+function getUpdatedJobs() {
+    $jobs = R::findAll('job');
+    foreach ($jobs as $job) {
+        $status = new Resque_Job_Status($job->token);
+        $job->status = $status->get();
+        R::store($job);
+    }
+
+    return $jobs;
+}
+
+// show form
 $app->get('/videos/new', function() use ($app) {
     $app->render('new.html'); 
 });
 
-$app->get('/video-upload-form', function() use ($app, $config) {
-    $aws = $config('amazon');
-    $bucket = $aws['bucket'];
-
-    $app->render('video-upload-form.html', [
-        'key' => '/users/iolloyd',
-        'acl' => 'public-read',
-        'success_redirect' => $_SERVER['HTTP_HOST'] . '/amazon_ok',
-        'amazonEndpoint' => getEndpoint($bucket),
-        'aws_access_key_id' => $aws['access_key'],
-    ]);
-});
-
 $app->get('/videos', function() use ($app) {
-    $videos = getVideos();
+    $videos = R::findAll('video');
     $app->json(R::exportAll($videos));
 });
 
 $app->get('/videos/:id', function($id) use ($app) {
-    $data = ['id' => $id];
-    $app->json(getVideo($id));
+    $video = R::load('video', $id);
+    $app->json(R::exportAll($video));
 });
 
 // Trigger upload to paysite
 $app->get('/videos/:id/upload/process', function($id) use ($app, $config) {
     $video = R::load('video', $id);
     $backend = $config('redis')['backend'];
-    $token = queueFileUpload($backend, $video, 'ilove.mp4');
+    $token = queueFileUpload($backend, $app->user, $video, 'ilove.mp4');
     echo $token;
 });
 
 // ADMIN
 $app->get('/admin/status', function() use ($app) {
-    $states = [
-        1 => 'waiting', 
-        2 => 'running', 
-        3 => 'failed',  
-        4 => 'complete'
-    ];
-
-    $jobs = R::findAll('job');
-    foreach ($jobs as $job) {
-        $status = new Resque_Job_Status($job->token);
-        $job->status = $states[$status->get()];
-        R::store($job);
-    }
+    $jobs = getUpdatedJobs();
 
     $latestJobs = array_slice($jobs, -10);
     $app->render('admin/status.html', ['jobs' => $latestJobs]);
