046e6df205313665045c372d2dfa22384a547e67
lloyd <manchesterboy@gmail.com>
2014-04-15 08:00:37 +0200

    phpcs line length

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+if (php_sapi_name() === 'cli-server' 
+    && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
+) {
     return false;
 }
 
@@ -14,7 +16,6 @@ require 'autoload.php';
 /*
  * Slim Application
  */
-
 ini_set('session.name', 'CLOUD');
 ini_set('session.cookie_httponly', true);
 
