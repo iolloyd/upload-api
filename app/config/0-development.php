d15e284da1da499841e76afee41df49c49f09293
lloyd <manchesterboy@gmail.com>
2014-04-16 15:18:23 +0200

    add more configuration for dev data

--- app/config/0-development.php
+++ app/config/0-development.php
@@ -26,4 +26,53 @@ $app->configureMode('development', function () use ($app) {
         's3.secret' => 'Upx55+HPpkqWDWrZyRWVkrZz5ElV1TxSFZyZVdOh',
         's3.region' => 'us-west-2',
     ]);
+
+    $app->config([
+        'dev.video' => 'some/test/video',
+        'dev.destination' => 'some/test/destination'
+    ]);
+
+    $app->config([
+        'dev.users' => [
+            [
+                'username' => 'dev',
+                'email' => 'dev@cloud.com',
+                'password' => 'password'
+            ],
+            [
+                'username' => 'nok',
+                'email' => 'other@cloud.com',
+                'password' => 'other' 
+            ]
+        ],
+
+        'dev.videos' => [
+            [
+                'path' => 'path',
+                'title' => 'Angie likes chickens', 
+                'description' => 'A video about feather love', 
+                'tags' => [
+                    'feathers', 'eggs', 'chickens', 'farms'
+                ]
+            ],
+            [
+                'path' => 'path',
+                'title' => 'Angie likes apples', 
+                'description' => 'A tale of fruity desires', 
+                'tags' => [
+                    'fruits', 'green', 'tempts', 'grows'
+                ]
+            ],
+            [
+                'path' => 'path',
+                'title' => 'Carol wants steak', 
+                'description' => 'Loving story of meaty madness',
+                'tags' => [
+                    'steak', 'gravy', 'yummy', 'succulent', 'protein'
+                ]
+            ]
+        ]
+    ]);
+
+
 });
