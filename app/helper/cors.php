76e22024c30292f2e1f9f1e44ef8bf64acf04bbd
Lloyd Moore <manchesterboy@gmail.com>
2014-04-27 19:49:53 +0200

    Added Flow.js chunked uploads

--- app/helper/cors.php
+++ app/helper/cors.php
@@ -7,8 +7,16 @@ $app->response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DE
 $app->response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-XSRF-TOKEN');
 $app->response->headers->set('Access-Control-Allow-Credentials', 'true');
 $app->response->headers->set('Access-Control-Max-Age', '3600');
+$app->response->headers->set('Vary', 'Origin, Access-Control-Request-Headers, Access-Control-Request-Method');
 
 $app->configureMode('development', function () use ($app) {
-    $app->response->headers->set('Access-Control-Allow-Origin', '*');
+    $app->response->headers->set('Access-Control-Allow-Origin', $app->request->headers->get('Origin'));
 });
 
+/**
+ * Always stop and send empty response for OPTIONS requests
+ */
+$app->options('.*', function () use ($app) {
+    $app->status(204);
+    $app->stop();
+});
