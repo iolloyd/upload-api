07b3e6b0fdbefca97f7387aa549d63824d0c2033
lloyd <manchesterboy@gmail.com>
2014-04-28 18:23:54 +0200

    update video model

--- src/Cloud/Model/Video.php
+++ src/Cloud/Model/Video.php
@@ -1,14 +1,110 @@
 <?php
 
 namespace Cloud\Model;
-use Cloud\Model\AbstractModel;
+use Doctrine\Common\Collections\ArrayCollection;
 
-class Video extends AbstractModel 
+/**
+ * @Entity @Table(name="video")
+ **/
+class Video
 {
-    protected $manyToMany = ['tag'];
+    const STATUS_PENDING = 'pending';
+    const STATUS_WORKING = 'working';
+    const STATUS_COMPLETE = 'complete';
+    const STATUS_ERROR = 'error';
 
-    public $path;
-    public $title;
-    public $description;
+    /** 
+     * @Id @Column(type="integer") @GeneratedValue 
+     */
+    protected $id;
+
+    /** 
+     * @Column(type="string)
+     */
+    protected $filename;
+
+    /** @ManyToMany(targetEntity="Tag") **/
+    protected $tags;
+
+    /**
+     * @Column(type="varchar")
+     */
+    protected $status;
+
+    /** 
+     * @OneToMany(targetEntity="VideoInbound", mappedBy="video") 
+     */
+    protected $videoInbounds;
+
+    /** 
+     * @OneToMany(targetEntity="VideoOutbound", mappedBy="video") 
+     * @JoinColumn(name="video_id", referencedColumnName="id") 
+     */
+    protected $videoOutbounds;
+
+    /**
+     * @ManyToOne(targetEntity="User", inversedBy="videos")
+     * @JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $creator;
+
+    public function __construct()
+    {
+        $this->tags = new ArrayCollection;
+        $this->videoInbounds = new ArrayCollection;
+        $this->videoOutbounds = new ArrayCollection;
+
+    }
+
+    public function setFilename($path)
+    {
+        $this->filename = $path;
+    }
+
+    public function setStatus($status)
+    {
+        if (!in_array($status, [
+            self::STATUS_PENDING,
+            self::STATUS_WORKING,
+            self::STATUS_COMPLETE,
+            self::STATUS_ERROR,
+        ])
+        ) {
+            throw new \InvalidArgumentException(
+                "Invalid Status"
+            );
+        }
+        $this->status = $status;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function addTag(Tag $tag)
+    {
+        $this->tags[] = $tag;
+    }
+
+    public function addInbound(VideoInbound $videoInbound)
+    {
+        $this->videoInbounds[] = $videoInbound;
+    }
+
+    public function addOutbound(VideoOutbound $videoOutbound)
+    {
+        $this->videoOutbounds[] = $videoOutbound;
+    }
 
 }
