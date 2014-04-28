5a0120fa7652e0d34913c9d2a170634f36213aa1
lloyd <manchesterboy@gmail.com>
2014-04-28 18:26:20 +0200

    updated models

--- src/Cloud/Model/User.php
+++ src/Cloud/Model/User.php
@@ -1,15 +1,62 @@
 <?php
 
 namespace Cloud\Model;
-use Cloud\Model\AbstractModel;
+use Doctrine\Common\Collections\ArrayCollection;
 
-class User extends AbstractModel
+/**
+ * @Entity @Table(name="user")
+ **/
+class User 
 {
-    protected $oneToMany = ['video'];
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
 
-    public $username;
-    public $email;
-    public $password;
+    /** 
+     * @ManyToOne(targetEntity="company", inversedBy="users")
+     * @JoinColumn(name="company_id", referencedColumnName="id")
+     */
+    protected $company;
+
+    /** @Column(type="string") **/
+    protected $email;
+
+    /** @Column(type="string") **/
+    protected $password;
+
+    /**
+     * @OneToMany(targetEntity="video", mappedBy="user")
+     */
+    protected $videos;
+
+    /**
+     * @OneToMany(targetEntity="Video", mappedBy="user")
+     */
+    public function __construct()
+    {
+        $this->videos = new ArrayCollection;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = crypt($password);
+    }
+
+    public function verifyPassword($guess)
+    {
+        $crypted = crypt($guess);
+        return crypt($guess, $crypted) == $this->password;
+    }
+
+    public function addVideo(Video $video)
+    {
+        $this->videos[] = $video;
+    }
+
 
 }
 
