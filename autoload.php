e16c4306fa202f806c513413ec0f8cf369f96a7d
lloyd <manchesterboy@gmail.com>
2014-04-28 18:58:42 +0200

    minor tidy

--- autoload.php
+++ autoload.php
@@ -9,9 +9,9 @@ if (!file_exists('vendor/autoload.php')) {
 $autoloader = require 'vendor/autoload.php';
 
 if (!class_exists('Slim\Slim')) {
-    throw new RuntimeException('Unable to load vendor libraries. Run `php composer.phar install`');
+    throw new RuntimeException(
+        'Unable to load vendor libraries. Run `php composer.phar install`'
+    );
 }
 
 $autoloader->add('Cloud\\', 'src/');
-
-require_once 'src/RedBean.phar';
