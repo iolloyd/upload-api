fe41587ac7dbb97ab172cc638c44aec84936499c
lloyd <manchesterboy@gmail.com>
2014-04-30 21:31:14 +0200

    add test for site

--- src/Tests/SiteTest.php
+++ src/Tests/SiteTest.php
@@ -3,9 +3,28 @@ namespace Tests;
 
 use Cloud\Model\Site;
 use Tests\Mock\MockSite;
+use Tests\Mock\MockVideoOutbound;
 
 class SiteTest extends Model
 {
+    public function testAddVideoOutbound()
+    {
+        $em = $this->entityManager;
+        $site = MockSite::get();
+        $videoOutbound = MockVideoOutbound::get();
+        $site->addVideoOutbound($videoOutbound);
+        $em->persist($site);
+        $em->flush();
+
+        $videoOutbounds = $site->getVideoOutbounds();
+
+        $expected = $videoOutbound;
+        $actual = $videoOutbounds[count($videoOutbounds) - 1];
+
+        $this->assertEquals($expected, $actual);
+    }
+
+
     public function testSave()
     {
         $em = $this->entityManager;
