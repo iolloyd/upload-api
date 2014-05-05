e5d0a1c36e8fba600d37c434a7ab13db11d82cbf
lloyd <manchesterboy@gmail.com>
2014-05-05 19:43:32 +0200

    Monolithic commit as requested

--- src/Cloud/Model/Traits/TimestampableTrait.php
+++ src/Cloud/Model/Traits/TimestampableTrait.php
@@ -1,31 +1,18 @@
 <?php
 
 namespace Cloud\Model\Traits;
+
+use Gedmo\Mapping\Annotation\Timestampable;
 use DateTime;
 
 /**
  * Trait for `$created_at` field
+ *
+ * Entity must declare `@HasLifecycleCallbacks` 
+ * for this trait to work as expected.
  */
 trait TimestampableTrait
 {
-    /**
-     * @Column(type="datetime")
-     */
-    protected $created_at;
-
-    /**
-     * Set the created date only the first time
-     *
-     * @PrePersist
-     */
-    public function prePersistSetCreatedAt()
-    {
-        if (empty($this->created_at)) {
-            $now = date('Y-m-d H:i:s');
-            $this->created_at = $now; 
-        }
-    }
-
     /**
      * Get the created date
      *
@@ -33,18 +20,7 @@ trait TimestampableTrait
      */
     public function getCreatedAt()
     {
-        return $this->created_at;
-    }
-
-
-    /**
-     * Set the updated date
-     * @PreUpdate
-     */
-    public function preUpdateUpdatedAt()
-    {
-        $now = date('Y-m-d H:i:s');
-        $this->updated_at = $date;
+        return $this->createdAt;
     }
 
     /**
@@ -52,8 +28,20 @@ trait TimestampableTrait
      */
     public function getUpdatedAt()
     {
-        return $this->updated_at;
+        return $this->updatedAt;
     }
+
+    /**
+     * @Column(type="datetime")
+     * @\Gedmo\Mapping\Annotation\Timestampable(on="create")
+     */
+    protected $createdAt;
+
+    /**
+     * @Column(type="datetime")
+     * @\Gedmo\Mapping\Annotation\Timestampable(on="create")
+     */
+    protected $updatedAt;
 
 }
 
