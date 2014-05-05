e63ea3befb9ed9c59150253385275f3679739319
lloyd <manchesterboy@gmail.com>
2014-05-05 19:41:14 +0200

    small refactor

--- src/Cloud/Model/DataFixtures/TubesiteFixture.php
+++ src/Cloud/Model/DataFixtures/TubesiteFixture.php
@@ -17,6 +17,15 @@ class TubesiteFixture extends AbstractFixture
      */
     public function load(ObjectManager $em)
     {
+        $youporn = $this->getYouPorn();
+        $xhamster = $this->getXhamster();
+        $em->persist($youporn);
+        $em->persist($xhamster);
+        $em->flush();
+    }
+
+    protected function getYouPorn()
+    {
         $youporn = new Tubesite();
         $youporn->setTitle('YouPorn');
         $youporn->setDescription('YouPorn is a 100% free streaming adult video site hosting hundreds of thousands of videos from every genre imaginable that services more than 15 million visitors worldwide every day.');
@@ -24,6 +33,11 @@ class TubesiteFixture extends AbstractFixture
         $youporn->setLoginUrl('http://www.youporn.com/upload/');
         $youporn->setSignupUrl('http://www.youporn.com/contentpartnerprogram/getstarted/');
 
+        return $youporn;
+    }
+
+    protected function getXhamster()
+    {
         $xhamster = new Tubesite();
         $xhamster->setTitle('xHamster');
         $xhamster->setDescription('Now you can earn money with xHamster promoting your content (videos and photos) to xHamsters users (~20,000,000 unique visitors/day). Posting on xHamster is 100% free, we earn only when you earn with your revshare program.');
