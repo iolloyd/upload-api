950c49fb673ee7ac22f6c1882c8e6431ef88a5bd
Alex Brausewetter <alex@xoob.de>
2014-05-09 13:01:10 +0200

    Add fixture for TubesiteUser #30

---
+++ src/Cloud/Model/DataFixtures/TubesiteUserFixture.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cloud\Model\DataFixtures;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Cloud\Model\TubesiteUser;
+
+/**
+ * Loads sample users without the credentials
+ */
+class TubesiteUserFixture extends AbstractFixture implements DependentFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $em)
+    {
+        $youpornUser = new TubesiteUser(
+            $this->getReference('youporn'),
+            $this->getReference('cumulus')
+        );
+
+        $youpornUser->setUsername('DaneJones');
+        $youpornUser->setPassword('...'); // update db manually
+        $youpornUser->setExternalId(4864148);
+        $youpornUser->setParam('content_partner_site_id', 2242);
+
+        $em->persist($youpornUser);
+        $em->flush();
+    }
+
+    /**
+     * Get the other fixtures this one is dependent on
+     */
+    public function getDependencies()
+    {
+        return [
+            __NAMESPACE__ . '\TubesiteFixture',
+            __NAMESPACE__ . '\CompanyFixture',
+        ];
+    }
+}
