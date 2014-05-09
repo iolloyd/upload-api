e3db63a933cb8c52fe18f3f89c13d062ebaeda35
Lloyd Moore <manchesterboy@gmail.com>
2014-05-09 13:31:23 +0200

    Bugfixes

--- src/Cloud/Model/Traits/TimestampableTrait.php
+++ src/Cloud/Model/Traits/TimestampableTrait.php
@@ -2,46 +2,24 @@
 
 namespace Cloud\Model\Traits;
 
-use Gedmo\Mapping\Annotation as Gedmo;
-use Doctrine\ORM\Mapping as ORM;
-
 use DateTime;
 
 /**
  * Trait for `$createdAt` field
- *
- * Entity must declare `@HasLifecycleCallbacks` 
- * for this trait to work as expected.
  */
 trait TimestampableTrait
 {
     /**
-     * @Gedmo\Timestampable(on="create")
-     * @ORM\Column(type="date")
+     * @Column(type="datetime")
+     * @\Gedmo\Mapping\Annotation\Timestampable(on="create")
      */
     protected $createdAt;
 
     /**
-     * @ORM\Column(type="datetime")
-     * @Gedmo\Timestampable(on="update")
+     * @Column(type="datetime")
+     * @\Gedmo\Mapping\Annotation\Timestampable(on="update")
      */
     protected $updatedAt;
-
-    /**
-     * @PrePersist
-     */
-    public function prePersistSetCreatedAt()
-    {
-        $this->createdAt = new \DateTime();
-    }
-
-    /**
-     * @PreUpdate
-     */
-    public function preUpdateSetUpdatedAt()
-    {
-        $this->updatedAt = new \DateTime();
-    }
 
     /**
      * Get created
