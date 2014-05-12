5293890a597c8ad68838bdd498b3f96c1b814746
Lloyd Moore <manchesterboy@gmail.com>
2014-05-12 18:31:05 +0200

    Slim to Silex WIP

---
+++ src/Cloud/Silex/Route.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * cloudxxx-api (http://www.cloud.xxx)
+ *
+ * Copyright (C) 2014 Really Useful Limited.
+ * Proprietary code. Usage restrictions apply.
+ *
+ * @copyright  Copyright (C) 2014 Really Useful Limited
+ * @license    Proprietary
+ */
+
+namespace Cloud\Silex;
+
+use Silex\Route as BaseRoute;
+use Silex\Route\SecurityTrait;
+
+class Route extends BaseRoute
+{
+    use SecurityTrait;
+}
