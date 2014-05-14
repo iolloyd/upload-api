d1bc75a385d10a8719c982362b733537d7feb137
lloyd <manchesterboy@gmail.com>
2014-05-14 11:21:56 +0200

    wip

--- app/routes/videos.php
+++ app/routes/videos.php
@@ -1,7 +1,16 @@
 <?php
+/**
+ * @package  cloudxxx-api (http://www.cloud.xxx)
+ *
+ * @author    ReallyUseful <info@ruseful.com>
+ * @copyright 2014 Really Useful Limited
+ * @license   Proprietary code. Usage restrictions apply.
+ */
 
 use Cloud\Model\Video;
 use Cloud\Model\VideoOutbound;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Get a video
@@ -17,21 +26,41 @@ $app->get('/videos/{video}', function(Video $video) use ($app)
 /**
  * Get list of videos
  */
-$app->get('/videos/{videos}', function($videos) use ($app)
+$app->get('/videos', function(Request $request) use ($app)
 {
-    return $app->json($videos);
-})
-->value('videos', 'videos')
-->convert('videos', 'converter.video:convertAll');
+    $first = $request->get('first') ?: 0;
+    $limit = $request->get('limit') ?: 10;
+
+    //$dql = "SELECT v, i from Cloud\Model\Video v JOIN v.inbounds i";
+    $dql = "SELECT v FROM Cloud\Model\Video v";
+    $qry = $app['em']->createQuery($dql)
+        ->setFirstResult($first)
+        ->setMaxResults($limit);
+
+    $videos = new Paginator($qry, $fetchjoinCollection = true);
+    $count = count($videos);
+
+    return $app->json([
+        'videos' => $videos, 
+        'count'  => $count,
+    ]);
+});
 
 /**
  * Create new draft video
  */
-$app->post('/videos', function() use ($app)
+$app->post('/videos', function(Request $request) use ($app)
 {
-    $video = new Video($app->session->user);
-
-    $video->setCompany($app->session->company());
+    $user = $app->session->user;
+    $video = new Video($user);
+    $video->setTitle($request->get('title'));
+    $video->setDescription($request->get('description'));
+    $video->setTags($request->get('tags'));
+    $video->setStatus($request->get('status'));
+    $video->setFilename($request->get('filename'));
+    $video->setFilesize($request->get('filesize'));
+    
+    // TODO inbounds? outbounds?
 
     $app->em->persist($video);
     $app->em->flush();
@@ -101,3 +130,147 @@ $app->post('/videos/{video}/publish', function(Video $video) use ($app)
 
     $app->json($video);
 });
+
+/**
+ * Create a new inbound upload and get parameters for the form to
+ * AWS S3
+ */
+$app->post('/videos/:video/inbounds', function(Video $video) use ($app)
+{
+    $inbound = new VideoInbound($video);
+
+    $app->em->persist($inbound);
+    $app->em->flush();
+
+    $form = new PostObject($app->s3, $app->config('s3.bucket'), [
+        'ttd'                             => '+24 hours',
+        'acl'                             => CannedAcl::PRIVATE_ACCESS,
+        'success_action_status'           => 200,
+
+        'key'                             => '^' . $inbound->getStorageChunkPath() . '/${filename}',
+
+        'x-amz-meta-cx-video'             => $video->getId(),
+        'x-amz-meta-cx-videoinbound'      => $inbound->getId(),
+        'x-amz-meta-cx-company'           => $video->getCompany()->getId(),
+
+        'x-amz-meta-flowchunknumber'      => '^',
+        'x-amz-meta-flowchunksize'        => '^',
+        'x-amz-meta-flowcurrentchunksize' => '^',
+        'x-amz-meta-flowtotalsize'        => '^',
+        'x-amz-meta-flowidentifier'       => '^',
+        'x-amz-meta-flowfilename'         => '^',
+        'x-amz-meta-flowrelativepath'     => '^',
+        'x-amz-meta-flowtotalchunks'      => '^',
+    ]);
+
+    $form->prepareData();
+
+    $json = [
+        'id'         => $inbound->getId(),
+        'video'      => ['id' => $video->getId()],
+        'form'       => $form->getFormAttributes(),
+        'fields'     => $form->getFormInputs(),
+        'file_field' => 'file',
+    ];
+
+    $json['fields'] = array_filter($json['fields']);
+
+    $app->json($json);
+})
+    ->convert('video', 'converter.video:convert');
+
+/**
+ * Complete chunk upload and combine chunks into single file
+ */
+$app->post('/videos/:video/inbounds/:videoinbound/complete',
+    function(Video $video, VideoInbound $inbound) use ($app)
+{
+    if ($inbound->getVideo() != $video) {
+        return $app->notFound();
+    }
+
+    if ($inbound->getStatus() != 'pending') {
+        return $app->jsonError(400, 'invalid_status', 'Inbound must have status `pending` to finalize');
+    }
+
+    // init
+
+    $app->em->transactional(function ($em) use ($inbound) {
+        $inbound->setStatus('working');
+    });
+
+    $upload = new FlowUpload(
+        $app->s3,
+        $app->config('s3.bucket'),
+        $inbound->getStorageChunkPath() . '/',
+        []
+    );
+
+    // validate
+
+    try {
+        $upload->validate();
+    } catch (RuntimeException $e) {
+        $app->em->transactional(function ($em) use ($inbound) {
+            $inbound->setStatus('error');
+        });
+
+        return $app->jsonError($e->getCode() ?: 400, 'invalid_upload', $e->getMessage());
+    }
+
+    // combine
+
+    $app->em->transactional(function ($em) use ($video, $inbound, $upload) {
+        $mimetypes = Mimetypes::getInstance();
+        $meta      = $upload->getMetadata();
+
+        $video->setFilename($meta['flowfilename']);
+        $video->setFilesize($meta['flowtotalsize']);
+        $video->setFiletype($mimetypes->fromFilename($meta['flowfilename']));
+
+        /*
+         * video.formats
+         *   id, format = [ raw, 720p, mobile, foo, bar ], filename, storage_path,
+         *   audio_codec, video_codec, ...
+         *
+         */
+
+        $upload->copyToObject(sprintf('videos/%d/raw/%s',
+            $video->getId(),
+            $video->getFilename()
+        ));
+
+        $upload->deleteChunks();
+
+        $inbound->setStatus('complete');
+    });
+
+    $app->status(204);
+})
+    ->convert('video', 'converter.video:convert')
+    ->convert('inbound', 'converter.inbound:convert');
+
+/**
+ * Abort chunk upload and delete chunks
+ */
+$app->delete('/videos/:video/inbounds/:videoinbound', function(Video $video, VideoInbound $inbound) use ($app)
+{
+    if ($inbound->getVideo() != $video) {
+        return $app->notFound();
+    }
+
+    if ($inbound->getStatus() != 'pending') {
+        return $app->jsonError(400, 'invalid_status', 'Inbound must have status `pending` to delete');
+    }
+
+    $upload = new FlowUpload(
+        $app->s3,
+        $app->config('s3.bucket'),
+        $key,
+        []
+    );
+
+    $app->json(iterator_to_array($upload->deleteChunks()));
+})
+    ->convert('video', 'converter.video:convert')
+    ->convert('inbound', 'converter.inbound:convert');
