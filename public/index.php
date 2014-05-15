3e86a3cde223ff4ea3dda61316e51e7eebbe25ca
lloyd <manchesterboy@gmail.com>
2014-05-15 09:59:46 +0200

    wip

--- public/index.php
+++ public/index.php
@@ -13,81 +13,8 @@ if (php_sapi_name() === 'cli-server'
     return false;
 }
 
-// Init autoloading
-require 'autoload.php';
-
-/*
- * Silex Application
- */
-
-$app = new Cloud\Silex\Application();
-$app['route_class'] = 'Cloud\Silex\Route';
-
-// env
-$app['env'] = 'development';
-$app['debug'] = ($app['env'] == 'development');
-
-// config
-$app->register(new Herrera\Wise\WiseServiceProvider(), [
-    'wise.path' => 'app/config/',
-]);
-$app['config'] = $app['wise']->load($app['env'] . '.ini');
-
-// db
-$app->register(new Silex\Provider\DoctrineServiceProvider(), [
-    'db.options' => $app['config']['db.options']
-
-]);
-
-$app->register(new Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider(), [
-    'orm.em.options' => [
-        'mappings' => [
-            [
-                'type'      => 'annotation',
-                'namespace' => 'Cloud\Model',
-                'path'      => 'src/Cloud/Model/',
-            ],
-        ],
-    ],
-]);
-
-$app['em'] = $app['orm.em'];
-
-// providers
-$app->register(new Silex\Provider\SecurityServiceProvider(), [
-    'security.firewalls' => [
-        'login' => [
-            'pattern' => '^/login$',
-        ],
-        'default' => [
-            'pattern' => '^/',
-            'anonymous' => true,
-            'users' => array(
-                // raw password is foo
-                'user' => array('ROLE_USER', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
-                'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
-            ),
-        ],
-    ],
-]);
-
-$app->register(new Silex\Provider\SessionServiceProvider(), [
-    'session.storage.options' => [
-        'name'            => 'CLOUD',
-        'cookie_lifetime' => $app['debug'] ? null : '2h',
-        'cookie_httponly' => true,
-    ],
-]);
-
-// loader
-$app->register(new Cloud\Silex\Loader(), [
-    'loader.path' => 'app/',
-    'loader.extensions' => [
-        'php',
-    ],
-]);
-$app['load']('helper');
-
+// Init Silex Application
+require 'bootstrap.php';
 // run
 $app->boot();
 $app['load']('routes');
---
+++ src/Cloud/Silex/ApplicationAwareInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Cloud\Silex;
+
+/**
+ * Interface for classes that depend on the Silex application
+ */
+interface ApplicationAwareInterface
+{
+    /**
+     * Set the Silex application
+     *
+     * This method injects the primary silex application instance into
+     * this class
+     *
+     * @param  \Silex\Silex $application
+     * @return SilexAwareInterface
+     */
+    public function setApplication(\Cloud\Silex\Application $app);
+
+    /**
+     * Get the Silex application
+     *
+     * @return \Cloud\Silex\Application
+     */
+    public function getApplication();
+}
