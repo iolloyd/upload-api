b42f1c47cf74727b0421e0d73792c0051877b318
lloyd <manchesterboy@gmail.com>
2014-05-15 18:08:30 +0200

    tidy up

--- app/helper/converter.php
+++ app/helper/converter.php
@@ -9,7 +9,16 @@ $app['converter'] = $app->protect(function($entityName) use ($app) {
     return new DoctrineOrmConverter($app['em'], $entityName);
 });
 
-$app['converter.video'] = $app['converter']('Cloud\Model\Video');
-$app['converter.outbound'] = $app['converter']('Cloud\Model\VideoOutbound');
-$app['converter.inbound'] = $app['converter']('Cloud\Model\VideoInbound');
+$app['converter.video'] = $app['converter'](
+    'Cloud\Model\Video'
+);
+
+$app['converter.inbound'] = $app['converter'](
+    'Cloud\Model\VideoInbound'
+);
+
+$app['converter.outbound'] = $app['converter'](
+    'Cloud\Model\VideoOutbound'
+);
+
 
