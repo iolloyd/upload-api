9f45a6ff663ec1a8a7139dd091cca232937035e1
lloyd <manchesterboy@gmail.com>
2014-05-15 18:10:22 +0200

    Add license comments

--- app/helper/converter.php
+++ app/helper/converter.php
@@ -1,5 +1,15 @@
 <?php
 /**
+ * cloudxxx-api (http://www.cloud.xxx)
+ *
+ * Copyright (C) 2014 Really Useful Limited.
+ * Proprietary code. Usage restrictions apply.
+ *
+ * @copyright  Copyright (C) 2014 Really Useful Limited
+ * @license    Proprietary
+ */
+
+/**
  * Parameter Converters
  */
 
