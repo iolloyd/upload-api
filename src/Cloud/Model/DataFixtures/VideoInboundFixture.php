ce2b1455573e3d28c502967ab4713898e881aaaa
lloyd <manchesterboy@gmail.com>
2014-05-15 15:30:05 +0200

    wip

---
+++ src/Cloud/Model/DataFixtures/VideoInboundFixture.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cloud\Model\DataFixtures;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Cloud\Model\Video;
+use Cloud\Model\VideoInbound;
+
+/**
+ * Loads all standard tubesites
+ */
+class VideoInboundFixture extends AbstractFixture implements DependentFixtureInterface   
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $em)
+    {
+        $inbound = new VideoInbound(
+            $this->getReference('video')
+        );
+
+        $inbound->setToken('token12345');
+        $inbound->setFilename('video inbound filename');
+        $inbound->setCreatedBy(
+            $this->getReference('user')
+        );
+
+        $em->persist($inbound);
+        $em->flush();
+
+        $this->addReference('video-inbound', $inbound);
+    }
+
+    public function getDependencies()
+    {
+        return [
+            __NAMESPACE__ . '\VideoFixture', 
+        ];
+    }
+
+}
