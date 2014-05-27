f3b957f3d1f55a87e4bd527c4f2ec4fb8587b27c
lloyd <manchesterboy@gmail.com>
2014-05-27 19:20:13 +0200

    security features

---
+++ src/Cloud/Model/Traits/CreatedByTrait.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * cloudxxx-api (http://www.cloud.xxx)
+ *
+ * Copyright (C) 2014 Really Useful Limited.
+ * Proprietary code. Usage restrictions apply.
+ *
+ * @copyright  Copyright (C) 2014 Really Useful Limited
+ * @license    Proprietary
+ */
+
+namespace Cloud\Model\Traits;
+
+use Cloud\Model\User;
+
+trait CreatedByTrait
+{
+    /**
+     * #JoinColumn(nullable=false)
+     * @ORM\ManyToOne(targetEntity="User")
+     * @JMS\Groups({"list", "details"})
+     * @CX\CreatedBy
+     */
+    protected $createdBy;
+
+    /**
+     * Set the user who created the model
+     *
+     * @param  User $user
+     * @return CreatedByTrait
+     */
+    public function setCreatedBy(User $user)
+    {
+        $this->createdBy = $user;
+        return $this;
+    }
+
+    /**
+     * Get the user who created the model
+     *
+     * @return User
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+}
