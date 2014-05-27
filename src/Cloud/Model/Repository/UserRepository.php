ccb952513bb2331953227555742b3ec74ff66447
lloyd <manchesterboy@gmail.com>
2014-05-27 19:27:02 +0200

    restore user repository

---
+++ src/Cloud/Model/Repository/UserRepository.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * cloudxxx-api (http://www.cloud.xxx)
+ *
+ * Copyright (C) 2014 Really Useful Limited.
+ * Proprietary code. Usage restrictions apply.
+ *
+ * @copyright  Copyright (C) 2014 Really Useful Limited
+ * @license    Proprietary
+ */
+
+namespace Cloud\Model\Repository;
+
+use Cloud\Model\User;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+class UserRepository extends EntityRepository
+    implements UserProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function loadUserByUsername($username)
+    {
+        $user = $this->findOneBy(['email' => $username]);
+
+        if (!$user) {
+            throw new UsernameNotFoundException(sprintf('User with email "%s" not found', $username));
+        }
+
+        return $user;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supportsClass($class)
+    {
+        return $this->getEntityName() == $class || is_subclass_of($class, $this->getEntityName());
+    }
+}
