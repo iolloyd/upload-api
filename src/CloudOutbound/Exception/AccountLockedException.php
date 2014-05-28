18f272e9abca9c4b21399759eb02c346b9cdf9cb
Alex Brausewetter <alex@xoob.de>
2014-05-28 16:51:47 +0200

    Add qualified exception classes

---
+++ src/CloudOutbound/Exception/AccountLockedException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * cloudxxx-api (http://www.cloud.xxx)
+ *
+ * Copyright (C) 2014 Really Useful Limited.
+ * Proprietary code. Usage restrictions apply.
+ *
+ * @copyright  Copyright (C) 2014 Really Useful Limited
+ * @license    Proprietary
+ */
+
+namespace CloudOutbound\Exception;
+
+class AccountLockedException extends \RuntimeException implements ExceptionInterface
+{
+}
