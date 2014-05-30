7e4d10a82f9516263ce233c3d5ab996f164bd383
lloyd <manchesterboy@gmail.com>
2014-05-30 10:50:01 +0200

    Clean up route for tubesites
    Signed-off-by: lloyd <manchesterboy@gmail.com>

--- app/routes/tubesites.php
+++ app/routes/tubesites.php
@@ -23,7 +23,7 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * Get a tubesite
  */
-$app->get('/tubesites/{tubesiteId}', function(Tubesite $tubesite) use ($app)
+$app->get('/tubesites/{tubesite}', function(Tubesite $tubesite) use ($app)
 {
     $groups = ['list', 'details.tubesite',];
     return $app['single.response.json']($tubesite, $groups);
@@ -41,40 +41,4 @@ $app->get('/tubesites', function(Request $request) use ($app)
     $pagedView = $app['paginator.response.json']('cx:tubesite', $groups);
     return $pagedView;
 });
-
-/**
- * Create new draft tubesite
- */
-$app->post('/tubesites', function(Request $request) use ($app)
-{
-    $tubesite = new Tubesite($app['user']);
-
-    $app['em']->persist($tubesite);
-    $app['em']->flush();
-
-    $groups = ['list', 'details.tubesites',];
-
-    return $app['single.response.json']($tubesite, $groups);
-});
-
-
-/**
- * Update a tubesite
- */
-$app->post('/tubesites/{tubesite}', function(Tubesite $tubesite, Request $request) use ($app)
-{
-    $app['em']->transactional(function () use ($app, $tubesite, $request) {
-        $tubesite->setTitle($request->get('title'));
-        $tubesite->setDescription($request->get('description'));
-        $tubesite->setFilename($request->get('filename'));
-        $tubesite->setFilesize($request->get('filesize'));
-        $tags = $app['converter.tags.from.request']($request->get('tags'));
-        $tubesite->setTags($tags);
-    });
-
-    $groups = ['list', 'details.tubesites',];
-
-    return $app['single.response.json']($tubesite, $groups);
-})
-    ->convert('tubesite', 'converter.tubesite:convert');
 
