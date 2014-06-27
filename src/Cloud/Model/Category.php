a349c07c1554abbe46775647868daa330c643c33
Lloyd Moore <manchesterboy@gmail.com>
2014-06-27 10:45:08 +0200

    DEV-14 Add category models

---
+++ src/Cloud/Model/Category.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * cloudxxx-api (http://www.cloud.xxx)
+ *
+ * Copyright (C) 2014 Really Useful Limited.
+ * Proprietary code. Usage restrictions apply.
+ *
+ * @copyright  Copyright (C) 2014 Really Useful Limited
+ * @license    Proprietary
+ */
+
+namespace Cloud\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use Cloud\Doctrine\Annotation as CX;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Category extends AbstractModel
+{
+    use Traits\IdTrait;
+    use Traits\SlugTrait;
+
+    /**
+     * @ORM\Column(type="string")
+     * @JMS\Groups({"list", "details"})
+     */
+    protected $title;
+
+    /**
+     * Gets the title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+      return $this->title;
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return Tag
+     */
+    public function setTitle($title)
+    {
+      $this->title = $title;
+    }
+}
+
