8b1da7bc37af8fbe029ea1127d546fe460a48f71
Alex Brausewetter <alex@xoob.de>
2014-06-27 08:40:15 +0200

    DEV-2 Add XVideos outbound uploader

--- src/Cloud/Model/DataFixtures/TubesiteFixture.php
+++ src/Cloud/Model/DataFixtures/TubesiteFixture.php
@@ -29,13 +29,16 @@ class TubesiteFixture extends AbstractFixture
     {
         $youporn = $this->getYouPorn();
         $xhamster = $this->getXhamster();
+        $xvideos = $this->getXVideos();
 
         $em->persist($youporn);
         $em->persist($xhamster);
+        $em->persist($xvideos);
         $em->flush();
 
         $this->addReference('youporn', $youporn);
         $this->addReference('xhamster', $xhamster);
+        $this->addReference('xvideos', $xvideos);
     }
 
     protected function getYouPorn()
@@ -44,7 +47,6 @@ class TubesiteFixture extends AbstractFixture
         $youporn->setTitle('YouPorn');
         $youporn->setDescription('YouPorn is a 100% free streaming adult video site hosting hundreds of thousands of videos from every genre imaginable that services more than 15 million visitors worldwide every day.');
         $youporn->setUrl('http://www.youporn.com/');
-        $youporn->setOrientation('straight');
         $youporn->setLoginUrl('http://www.youporn.com/upload/');
         $youporn->setSignupUrl('http://www.youporn.com/contentpartnerprogram/getstarted/');
 
@@ -56,7 +58,6 @@ class TubesiteFixture extends AbstractFixture
         $xhamster = new Tubesite();
         $xhamster->setTitle('xHamster');
         $xhamster->setDescription('Now you can earn money with xHamster promoting your content (videos and photos) to xHamsters users (~20,000,000 unique visitors/day). Posting on xHamster is 100% free, we earn only when you earn with your revshare program.');
-        $xhamster->setOrientation('solo');
         $xhamster->setUrl('http://xhamster.com/');
         $xhamster->setLoginUrl('http://upload.xhamster.com/producer.php');
         $xhamster->setSignupUrl('http://xhamster.com/content_program.php');
@@ -64,4 +65,16 @@ class TubesiteFixture extends AbstractFixture
         return $xhamster;
     }
 
+    protected function getXVideos()
+    {
+        $xvideos = new Tubesite();
+
+        $xvideos->setTitle('XVideos');
+        $xvideos->setDescription('XVideos is a free pornographic video sharing website. It is the most popular pornographic website in the world, having overcome Pornhub as most popular adult video sharing website in November 2010.');
+        $xvideos->setUrl('http://www.xvideos.com/');
+        $xvideos->setLoginUrl('http://upload.xvideos.com/account');
+        $xvideos->setSignupUrl('http://upload.xvideos.com/account/create');
+
+        return $xvideos;
+    }
 }
