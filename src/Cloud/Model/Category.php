c8f867846163c8b72bd7c0e4b47036c5ae5f8995
lloyd <manchesterboy@gmail.com>
2014-07-01 14:08:44 +0200

    Refactoring
    Signed-off-by: lloyd <manchesterboy@gmail.com>

--- src/Cloud/Model/Category.php
+++ src/Cloud/Model/Category.php
@@ -5,8 +5,13 @@
  * Copyright (C) 2014 Really Useful Limited.
  * Proprietary code. Usage restrictions apply.
  *
+<<<<<<< HEAD
  * @copyright  Copyright (C) 2014 Really Useful Limited
  * @license    Proprietary
+=======
+ * @copyright 2014 ReallyUsefulLimited
+ * @license   Proprietary
+>>>>>>> Refactoring
  */
 
 namespace Cloud\Model;
@@ -30,6 +35,15 @@ class Category extends AbstractModel
      */
     protected $title;
 
+<<<<<<< HEAD
+=======
+
+    public function __toString()
+    {
+        return (string) $this->id;
+    }
+
+>>>>>>> Refactoring
     /**
      * Gets the title
      *
@@ -43,11 +57,21 @@ class Category extends AbstractModel
     /**
      * @param string $title
      *
+<<<<<<< HEAD
      * @return Tag
+=======
+     * @return Category
+>>>>>>> Refactoring
      */
     public function setTitle($title)
     {
       $this->title = $title;
+<<<<<<< HEAD
     }
+=======
+      return $this;
+    }
+
+>>>>>>> Refactoring
 }
 
