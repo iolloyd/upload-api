b5f745d9f228702fe7dbb699aa46883980bae684
lloyd <manchesterboy@gmail.com>
2014-07-16 15:56:02 +0200

    rewrite configs and refactor classes

---
+++ src/CloudOutbound/XVideos/CategoryMapper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * cloudxxx-api (http://www.cloud.xxx)
+ *
+ * Copyright (C) 2014 Really Useful Limited.
+ * Proprietary code. Usage restrictions apply.
+ *
+ * @copyright  Copyright (C) 2014 Really Useful Limited
+ * @license    Proprietary
+ */
+
+namespace CloudOutbound\XVideos;
+
+use CloudOutbound\AbstractCategoryMapper;
+
+class CategoryMapper extends AbstractCategoryMapper
+{
+    protected $config = 'data/categories/xvideos.csv';
+}
+
+
