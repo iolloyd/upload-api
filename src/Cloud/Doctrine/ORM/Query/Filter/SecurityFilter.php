ef06a16d6e6832d55c998f4d16467725e5817dee
Lloyd Moore <manchesterboy@gmail.com>
2014-07-25 16:52:46 +0200

    Fix security SQL filter

--- src/Cloud/Doctrine/ORM/Query/Filter/SecurityFilter.php
+++ src/Cloud/Doctrine/ORM/Query/Filter/SecurityFilter.php
@@ -11,6 +11,8 @@
 
 namespace Cloud\Doctrine\ORM\Query\Filter;
 
+use InvalidArgumentException;
+use Cloud\Doctrine\SecurityEventSubscriber;
 use Cloud\Doctrine\Internal\ClassMetadataUtils as Utils;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Query\Filter\SQLFilter;
@@ -22,29 +24,56 @@ use Doctrine\ORM\Query\Filter\SQLFilter;
  */
 class SecurityFilter extends SQLFilter
 {
-    const FLAG_COMPANY = 'cx:identity:company';
-
     /**
      * {@inheritDoc}
      */
     public function addFilterConstraint(ClassMetadata $metadata, $targetTableAlias)
     {
-        if (!Utils::hasMetadataClassFlag($metadata, self::FLAG_COMPANY)) {
+        if (!Utils::hasMetadataClassFlag($metadata, SecurityEventSubscriber::FLAG_COMPANY)) {
             return '';
         }
 
         $filter = [];
 
         foreach ($metadata->associationMappings as $fieldName => $mapping) {
-            if (Utils::hasMetadataFieldFlag($metadata, $fieldName, self::FLAG_COMPANY)) {
-                $filter[] = $targetTableAlias
-                    . '.'
-                    . $metadata->getSingleAssociationJoinColumnName($fieldName)
-                    . ' = '
-                    . $this->getParameter('company_id');
+            if (Utils::hasMetadataFieldFlag($metadata, $fieldName, SecurityEventSubscriber::FLAG_COMPANY)) {
+                $companyId = $this->getCompanyId();
+
+                if ($companyId !== null) {
+                    $filter[] =
+                        $targetTableAlias
+                        . '.' . $metadata->getSingleAssociationJoinColumnName($fieldName)
+                        . ' = ' . $companyId
+                    ;
+                } elseif (Utils::getMetadataFieldFlag($metadata, $fieldName, SecurityEventSubscriber::FLAG_ALLOW_ANONYMOUS)) {
+                    /*
+                     * Don't filter if no company_id set (not logged in) but
+                     * allowAnonymous = true
+                     */
+                    continue;
+                } else {
+                    $filter[] =
+                        $targetTableAlias
+                        . '.' . $metadata->getSingleAssociationJoinColumnName($fieldName)
+                        . ' IS NULL'
+                    ;
+                }
             }
         }
 
         return implode(' AND ', $filter);
+    }
+
+    /**
+     * @return null|string  quoted value or null if the parameter has not been
+     *                       set
+     */
+    protected function getCompanyId()
+    {
+        try {
+            return $this->getParameter('company_id');
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
     }
 }
