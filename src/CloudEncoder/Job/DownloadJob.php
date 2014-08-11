b6880b077ad7fdd93ff8b1ee03bcd6d637be4ab6
lloyd <manchesterboy@gmail.com>
2014-08-11 14:38:53 +0200

    renaming

--- src/CloudEncoder/Job/DownloadJob.php
+++ src/CloudEncoder/Job/DownloadJob.php
@@ -12,10 +12,9 @@
 namespace CloudEncoder\Job;
 
 use Cloud\Job\AbstractJob;
-use CloudEncoder\VideoDownload;
+use CloudEncoder\S3VideoDownload;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -32,16 +31,27 @@ class DownloadJob extends AbstractJob
         $this
             ->setDefinition([
                 new InputArgument('input',  InputArgument::REQUIRED, 'The video url to get thumbnails for'),
+                new InputArgument('output', InputArgument::REQUIRED, 'The local path to store the downloaded video'),
             ])
             ->setName('job:encoder:download')
         ;
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $videoFile = $input->getArgument('input');
-        $downloader = new VideoDownload();
-        $downloader->process($videoFile);
+        $app    = $this->getHelper('silex')->getApplication();
+        $s3     = $app['aws']['s3'];
+        $bucket = $app['config']['aws']['bucket'];
+        $input  = $input->getArgument('input');
+        $output = $input->getArgument('output');
+
+        $downloader = new S3VideoDownload();
+        $downloader->process($s3, $bucket, $input, $output);
     }
 }
 
