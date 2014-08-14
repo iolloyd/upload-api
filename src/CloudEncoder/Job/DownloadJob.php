7feca0fc4a74dbe57a0f78a5e38321512bc038a5
lloyd <manchesterboy@gmail.com>
2014-08-14 14:39:50 +0200

    some formattting
    add note
    
    formatting
    
    clean up comments and make validation more robust and helpful

--- src/CloudEncoder/Job/DownloadJob.php
+++ src/CloudEncoder/Job/DownloadJob.php
@@ -17,23 +17,19 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Class VideoEncoder
- *
- */
 class DownloadJob extends AbstractJob 
 {
     /**
-     * Configures this job
+     * {@inheritdoc}
      */
     protected function configure()
     {
         $this
-            ->setDefinition([
-                new InputArgument('input',  InputArgument::REQUIRED, 'The url of the video to download'),
-                new InputArgument('output', InputArgument::REQUIRED, 'The local path to store the video'),
-            ])
             ->setName('job:encoder:download')
+            ->setDescription('Allows fetching of a video file from a given url or path')
+
+            ->addArgument('input',  InputArgument::REQUIRED, 'The video url to download from')
+            ->addArgument('output', InputArgument::REQUIRED, 'The local path to save to')
         ;
     }
 
@@ -42,12 +38,15 @@ class DownloadJob extends AbstractJob
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $infile = $input->getArgument('input');
+        $infile  = $input->getArgument('input');
         $outfile = $input->getArgument('output');
-        $client = new Client();
-        $result = $client->get($infile, [ 'save_to' => $outfile]);
+        $client  = new Client();
+        $result  = $client->get($infile, [
+            'save_to' => $outfile,
+        ]);
 
         return $result;
+
     }
 }
 
