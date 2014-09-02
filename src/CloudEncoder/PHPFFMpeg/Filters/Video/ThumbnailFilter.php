2a804c0efe0ad1417baabeb3772d78b46843820d
lloyd <manchesterboy@gmail.com>
2014-09-02 11:14:25 +0200

    Allow three overall methods of selecting thumbnails
    Signed-off-by: lloyd <manchesterboy@gmail.com>

--- src/CloudEncoder/PHPFFMpeg/Filters/Video/ThumbnailFilter.php
+++ src/CloudEncoder/PHPFFMpeg/Filters/Video/ThumbnailFilter.php
@@ -27,11 +27,10 @@ class ThumbnailFilter implements VideoFilterInterface
      * @param int $count
      * @param int $priority
      */
-    public function __construct($count = 1, $firstFrame = true, $priority = 0)
+    public function __construct(array $params, $priority = 0)
     {
-        $this->count      = $count;
-        $this->firstFrame = $firstFrame;
-        $this->priority   = $priority;
+        $this->params = $params;
+        $this->priority = $priority;
     }
 
     /**
@@ -49,8 +48,80 @@ class ThumbnailFilter implements VideoFilterInterface
      */
     public function apply(Video $video, VideoInterface $format)
     {
-        $fpsCommand = sprintf("fps=fps=%.2f", $this->count / 60);
-        return ['-f', 'image2', '-vf', $fpsCommand, 'thumb%d.jpg',];
+        $videoDuration = $video->getStreams()->videos()->first()->get('duration');
+
+        // Avoid making duplicate thumbnails
+        $times = array_unique(array_merge(
+            $this->params['thumbnail_times'],
+            $this->getIntervalsOrNumber($videoDuration)
+        ));
+
+        $commandArguments = [];
+
+        // Since array_unique preserves the keys, we need 
+        // to re-index the $times array
+        foreach (array_values($times) as $index => $time) {
+            $commandArguments = array_merge(
+                $commandArguments, 
+                ['-ss', $time, '-f', 'image2', '-vframes', '1', 'thumb'.$index.'.png']
+            );
+        }
+
+        return $commandArguments;
+    }
+
+    /**
+     * @param $videoDuration
+     * @return array
+     */
+    protected function getIntervalsOrNumber($videoDuration)
+    {
+        // TODO later we will allow both 
+        return (isset($this->params['thumbnail_interval']))
+            ? $this->getIntervals($videoDuration)
+            : $this->getNumber($videoDuration)
+        ;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getIntervals($videoDuration)
+    {
+        if (!$this->params['thumbnail_interval']) {
+            return [];
+        }
+
+        $interval = $this->params['thumbnail_interval'];
+        $start    = isset($this->params['thumbnail_first']) ? 0 : $interval;
+        $times    = range($start, $videoDuration, $interval);
+
+        return $times;
+    }
+
+    /**
+     * @param $videoDuration
+     * @return array
+     */
+    protected function getNumber($videoDuration)
+    {
+        if (!$this->params['thumbnail_number']) {
+            return [];
+        }
+
+        $divider  = $this->params['thumbnail_number'];
+        if (!$this->params['thumbnail_first']) {
+            ++$divider;
+        }
+
+        $period  = $videoDuration / $divider;
+        $times = range(0, $videoDuration-1, $period);
+
+        if (!$this->params['thumbnail_first']) {
+            array_shift($times);
+        }
+
+        return $times;
     }
 }
 
